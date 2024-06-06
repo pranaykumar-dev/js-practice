2b3c05a75b712afced0c40407af70889cbacdc6e
prani14 <pranaykumarshivas1@gmail.com>
2024-06-06 12:17:35 +0530

    date and sting manipulation

---
+++ 01_basics/date.js
@@ -0,0 +1,4 @@
+const myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleTimeString());
---
+++ 01_basics/string.js
@@ -0,0 +1,8 @@
+const userName = new String("hello")
+const userId = new Number(22)
+console.log(`user name is ${userName} user id is ${userId}`);
+console.log(userId);
+console.log(userName);
+console.log(userName.length);
+console.log(userName.lastIndexOf("o"));
+console.log(userName.toUpperCase());
