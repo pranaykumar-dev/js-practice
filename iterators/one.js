35ddf6ebda1adc5bbbd520066d4a6e4de986bd8f
prani14 <pranaykumarshivas1@gmail.com>
2024-06-13 13:23:59 +0530

    iterators

---
+++ iterators/one.js
@@ -0,0 +1,24 @@
+// for lopps
+for (let i = 0; i <= 10; i++) {
+    const element =i;
+    console.log(element);
+    
+}
+
+// multiple inheritence
+// for (i = 0; i <= 10; i++) {
+//     const element = i ;
+//     console.log(element);
+//     for (let j = 0; j < 10; j++) {
+//         const element = j;
+//         console.log(`${i} * ${j} = `, i *j );
+        
+//     }
+    
+// }
+const MyArray = ["pranay", "kumar"]
+for (let index = 0; index < MyArray.length; index++) {
+    const element = MyArray[index];
+    console.log(element);
+    
+}
---
+++ iterators/two.js
@@ -0,0 +1,18 @@
+//  const is not useful to do increment so use of let is useful
+
+MyArray = ["flash","jaibheem","ramji"]
+let arr = 0
+while (arr < MyArray.length) {
+    console.log(`the value is ${MyArray[arr]}`);
+     arr ++
+    
+}
+
+ 
+let i = 0
+do {
+    
+    console.log(`score is ${i}`);
+    i++
+    
+} while (i <= 10);
